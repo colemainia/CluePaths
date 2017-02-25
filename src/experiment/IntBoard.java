5d5fb8b4d059a03cd4e926c406819bcccff8a7a6
colemainia <choyt@mymail.mines.edu>
2017-02-24 20:52:37 -0700

    Adding methods to IntBoard

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -4,6 +4,16 @@ import java.util.Map;
 import java.util.Set;
 
 public class IntBoard {
+	public IntBoard(BoardCell[][] grid) {
+		this.grid = grid;
+		
+	}
+	private Map<BoardCell, Set<BoardCell>> calcAdjacencies() {
+		return adjMtx;
+	}
+	private Set<BoardCell> calcTargets(BoardCell startCell, int pathLength) {
+		return targets;
+	}
 	private Map<BoardCell, Set<BoardCell>> adjMtx;
 	private Set<BoardCell> visited;
 	private Set<BoardCell> targets;
