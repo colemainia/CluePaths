1e4cad77169bd9726b19b4f602909d12346db10c
Coleman Hoyt <choyt@mines.edu>
2017-03-01 15:54:09 -0700

    New branch

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -3,12 +3,20 @@ package clueGame;
 import java.util.Set;
 
 public class BoardCell {
+	public enum DoorDirection {
+		UP, DOWN, LEFT, RIGHT, NONE;
+	}
 	private int row;
 	private int column;
+	private char initial;
 	public BoardCell(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
+	public boolean isWalkway() {return true;}
+	public boolean isRoom() {return true;}
+	public boolean isDoorway() {return true;}
+	
 	public int GetRow() {return row;}
 	public int GetColumn() {return column;}
 }
